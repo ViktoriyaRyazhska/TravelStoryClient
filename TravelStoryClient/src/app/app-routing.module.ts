c762994dbe3cc9416038e580e06b3a37bdb562f3
Kostyk1999 <pryzyhlei@gmail.com>
2018-09-30 13:57:17 +0300

    Front end improving in chat module

--- TravelStoryClient/src/app/app-routing.module.ts
+++ TravelStoryClient/src/app/app-routing.module.ts
@@ -2,15 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {MainComponent} from "./main/main.component";
-import {MessengerComponent} from "./messenger/messenger.component";
-import {TestWebSocketsComponent} from "./test-web-sockets/test-web-sockets.component";
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'messenger', redirectTo:"messenger/chat/1"},
-  {path: 'messenger/chat/:id', component: MessengerComponent},
-  {path: 'testwebsocket', component: TestWebSocketsComponent}
-
 ];
 
 @NgModule({
--- TravelStoryClient/src/app/app.module.ts
+++ TravelStoryClient/src/app/app.module.ts
@@ -1,39 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import {MessengerComponent} from "./messenger/messenger.component";
-import {PersonalInfoComponent} from "./messenger/messenger-sidebar/personal-info/personal-info.component";
-import {ChatSidebarComponent} from "./messenger/messenger-sidebar/chat-sidebar.component";
-import {ChatListComponent} from "./messenger/messenger-sidebar/chat-list/chat-list.component";
-import {MainMessagingContentComponent} from "./messenger/main-messaging-content/main-messaging-content.component";
-import {ChatInfoComponent} from "./messenger/main-messaging-content/chat-info/chat-info.component";
-import { TestWebSocketsComponent } from './test-web-sockets/test-web-sockets.component';
-import { AppRoutingModule } from './app-routing.module';
-import { MainComponent } from './main/main.component';
+import {AppRoutingModule} from './app-routing.module';
+import {MainComponent} from './main/main.component';
 import {RouterModule} from "@angular/router";
-import { ChatSearchPipe } from './messenger/pipes/chat-search.pipe';
+import {MessengerModule} from "./messenger/messenger.module";
+
 // import {BsDropdownModule, ModalModule, TooltipModule} from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessengerComponent,
-    PersonalInfoComponent,
-    ChatSidebarComponent,
-    ChatListComponent,
-    MainMessagingContentComponent,
-    ChatInfoComponent,
-    TestWebSocketsComponent,
-    MainComponent,
-    ChatSearchPipe
+    MainComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    MessengerModule
     // BsDropdownModule.forRoot(),
     // TooltipModule.forRoot(),
     // ModalModule.forRoot()
