b65abafcd7d761198555f8010199d6f400972f61
Kostyk1999 <pryzyhlei@gmail.com>
2018-10-02 18:42:13 +0300

    Basic socket messenger is ready

--- TravelStoryClient/src/app/messenger/services/messenger.service.ts
+++ TravelStoryClient/src/app/messenger/services/messenger.service.ts
@@ -1,45 +1,63 @@
 import {Injectable} from '@angular/core';
 import {User} from "../model/User";
-import {chats} from "../mockups/chatsMockup";
-import {forEach} from "../../../../node_modules/@angular/router/src/utils/collection";
 import {Chat} from "../model/Chat";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
 import {Message} from "../model/Message";
-import {currUserMockup} from "../mockups/UserMockup";
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+const HARDCODED_USER_ID = 1;
+const colors: string[] = [
+  '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+  '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessengerService {
 
-  getChats(user: User) {
-    return chats;
+  baseUrl: string = "http://localhost:8080/";
+
+  constructor(private http: HttpClient) {
   }
 
-  getChat(id: number) {
-    let resChat: Chat;
-    for (let chat of chats) {
-      if (chat.id == id) {
-        resChat = chat;
-      }
+  getChats(user: User): Observable<Chat[]> {
+    return this.http.get<Chat[]>(this.baseUrl + "messenger/1/chats");
+  }
+
+  getChat(id: number): Observable<Chat> {
+    return this.http.get<Chat>(this.baseUrl + "messenger/chat/" + id);
+  }
+
+  getCurrentUser(): Observable<User> {
+    return this.http.get<User>(this.baseUrl + "messenger/user/" + HARDCODED_USER_ID);
+  }
+
+  getMessages(chatId: number, pageNumber: number): Observable<Message[]> {
+    let httpParams = new HttpParams().set('pageNumber', pageNumber.toString());
+    return this.http.get<Message[]>(this.baseUrl + "messenger/chat/" + chatId + "/messages",
+      {params: httpParams});
+  }
+
+  getInterlocutor(userId: number): Observable<User> {
+    return this.http.get<User>(this.baseUrl + "messenger/user/" + userId);
+  }
+
+  setInterlocutor(chat: Chat, currentUser: User) {
+
+  }
+
+  public getAvatarColor(chatName: string): string {
+    var hash = 0;
+    for (var i = 0; i < chatName.length; i++) {
+      hash = 31 * hash + chatName.charCodeAt(i);
     }
-    return resChat;
-  }
-
-  getMessages(chatId: number) {
-    let resChat: Chat;
-    for (let chat of chats) {
-      if (chat.id == chatId) {
-        resChat = chat;
-      }
-    }
-    debugger;
-    return resChat.messages;
-  }
-
-  getCurrentUser() {
-    return currUserMockup;
-  }
-
-  constructor() {
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
   }
 }
