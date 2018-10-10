96e8e62cb9641a297f3e0fda690809b9ef3432e6
Roma Shevchuk <shevromanvk@gmail.com>
2018-10-10 20:51:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const app = express();
 
-app.listen(process.env.PORT || 8080);
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'travelStoryClient'));
-
+// app.use(express.static(__dirname + 'travelStoryClient'));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.get('/*', function (req, res) {
 
   res.sendFile(path.join(__dirname + '/dist/travelStoryClient/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
