bf3d8bbceb2583c0de0cd3bc787472c52971d668
Roma Shevchuk <shevromanvk@gmail.com>
2018-10-10 17:50:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 
 });
 
