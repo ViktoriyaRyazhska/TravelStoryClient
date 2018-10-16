8703d70073006750a2c60d3e083cfacc52459d54
Roma Shevchuk <shevromanvk@gmail.com>
2018-10-16 14:15:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const app = express();
 
-
 // Serve only the static files form the dist directory
 app.use(express.static(path.join(__dirname, '/dist/travelStoryClient')));
 app.get('/*', function(req, res) {
