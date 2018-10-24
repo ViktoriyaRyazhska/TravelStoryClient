616d6eebb4da867ad9e110e3a86f60a9a8cae755
Roma Shevchuk <shevromanvk@gmail.com>
2018-10-24 21:20:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(path.join(__dirname, '/dist')));
 app.get('*', function(req, res) {
-  res.sendFile(__dirname, path.join('index.html'), function(err) {
+  res.sendFile(__dirname, path.join('/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
