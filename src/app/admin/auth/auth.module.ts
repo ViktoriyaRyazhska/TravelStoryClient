c5c296945ce9ab8af0176832853337d451063699
Oleh Krutiak <okrutiak@gmail.com>
2018-10-29 21:02:39 +0200

    deleted perfect scroll from auth module

--- src/app/admin/auth/auth.module.ts
+++ src/app/admin/auth/auth.module.ts
@@ -8,16 +8,10 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatTabsModule} from '@angular/material';
-// import {SidemenuComponent} from '../../core/sidemenu/sidemenu.component';
 
-import {PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
 import {DashboardCrmModule} from '../dashboard-crm/dashboard-crm.module';
 import {CoreModule} from '../../core/core.module';
 import {appRoutes} from './lazyloader.routes';
-
-const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
-  suppressScrollX: true
-};
 
 
 @NgModule({
@@ -30,16 +24,9 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     MatTabsModule,
     CoreModule,
     MatSidenavModule,
-    // PerfectScrollbarModule,
     DashboardCrmModule
   ],
-  declarations: [AuthComponent],
-  providers: [
-    {
-      provide: PERFECT_SCROLLBAR_CONFIG,
-      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
-    }
-  ]
+  declarations: [AuthComponent]
 })
 export class AuthModule {
 }
