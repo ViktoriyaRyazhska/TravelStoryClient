c65915ad74443c9d26bc04a20376ba42b841a01c
Oleh Krutiak <okrutiak@gmail.com>
2018-10-29 18:45:06 +0200

    admin-routing fixes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,24 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {AuthComponent} from './auth/auth.component';
 import {AdminComponent} from './admin.component';
 import {DashboardCrmComponent} from './dashboard-crm/dashboard-crm.component';
 
 const adminRoutes: Routes = [
   {path: 'admin', component: AdminComponent},
-  {
-    // {path: 'admin/chart', component: ChartsComponent}
-    path: '',
-    component: AuthComponent,
-    children: [
-      {path: 'auth1', component: AuthComponent},
-      {path: 'dashboard', component: DashboardCrmComponent}
-      // {path: 'settings', component: SettingsPageComponent},
-      // {path: 'user/:id', component: UserPageComponent},
-      // {path: 'login', component: LoginComponent},
-    ]
-  }
+  {path: 'admin/dashboard', component: DashboardCrmComponent}
 ];
 
 @NgModule({
