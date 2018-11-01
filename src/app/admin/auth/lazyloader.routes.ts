e644a3236490b44f29a7e145aab897b7a30f7e92
Oleh Krutiak <okrutiak@gmail.com>
2018-11-01 19:16:39 +0200

    change lazyloader

--- src/app/admin/auth/lazyloader.routes.ts
+++ src/app/admin/auth/lazyloader.routes.ts
@@ -1,10 +1,8 @@
 import {Routes} from '@angular/router';
 import {AuthComponent} from './auth.component';
-import {DashboardCrmComponent} from '../dashboard-crm/dashboard-crm.component';
 
 export const appRoutes: Routes = [{
   path: '', component: AuthComponent, children: [
-    {path: 'dashboard', component: DashboardCrmComponent},
     {path: 'tables', loadChildren: '../tables/tables.module#TablesModule'},
     {path: 'maps', loadChildren: '../maps/maps.module#MapsModule'},
     {path: 'pages', loadChildren: '../pages/pages.module#PagesModule'}
