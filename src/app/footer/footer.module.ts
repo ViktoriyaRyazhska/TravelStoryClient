8e251171b858cbc6d645d2fefb99a8582ba1d286
mdzhura <32909428+mdzhura@users.noreply.github.com>
2018-11-05 13:51:26 +0200

    Added dependency mat-sidenav to Header module

--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule
   ],
   declarations: []
 })
